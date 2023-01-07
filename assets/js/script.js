4094677f6d201b93e52cdc279e293c37c0bd5a77
BlueBindy <fionajthompson@outlook.com>
2023-01-07 19:39:37 +0000

    Fix bug in reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-function check_reset() {
+
+function checkReset() {
+    let confirmReset = document.getElementById("reset-button");
     return confirm("Are you sure you want to clear everything you've entered?");
-}
+    confirmReset.reset();
+} 
+
