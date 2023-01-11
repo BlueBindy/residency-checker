e7acb92b11240689d81137111ea1e759c12236c2
BlueBindy <fionajthompson@outlook.com>
2023-01-11 16:57:51 +0000

    Add: JS function to collect residency days by year

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@
 function submitButton(){
     /* days-section does not take .value as it is class name for a div which doesn't have a value, so throws undefined*/
    let allDays = document.getElementsByClassName("days-section");
-   let inputTag =  document.getElementsByTagName('inputfield-1');
-console.log(inputTag);
-   console.log(allDays);
+   let inputTag =  document.getElementsByClassName('input-field');
+   for (let i = 0; i < inputTag.length; i++) {
+     let findResult = parseInt(inputTag[i].value);
+     console.log(findResult)
+  }
 }
 
 /* change id to inputfield-n, for loop through that create a string inputfield-n.value*/
